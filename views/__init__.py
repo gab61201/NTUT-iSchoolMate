b20752dcd1bf7b0e588c63e3052beb312f2cb862
gab61201 <gab61201@gmail.com>
2025-10-01 21:32:01 +0800

    style(rename files): rename exit.py/home.py/login.py

--- views/__init__.py
+++ views/__init__.py
@@ -1,9 +1,9 @@
-from .home_page import home_page
-from .login_page import login_page
-from .exit_page import exit_page
+from .home import home_page
+from .login import login_page
+from .exit import exit_page
 
 __all__ = [
-    "home_page",
-    "login_page",
-    "exit_page"
+    "home",
+    "login",
+    "exit"
     ]
