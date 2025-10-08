ff2b93d61571f6fcb1234ab40c6bd14f22086c29
gab61201 <gab61201@gmail.com>
2025-10-08 21:01:00 +0800

    feat(home): course rendering and UI structure

--- views/home_components/right_panel.py
+++ views/home_components/right_panel.py
@@ -3,17 +3,37 @@ from typing import Literal, Any
 from modules.course import Course
 
 
-def render_home():
+def render_default():
     ...
 
-def render_course(course: Course):
+async def render_course(course: Course):
+    await course.fetch_syllabus()
+    await course.fetch_description()
+
+    ui.label(f"{course.seme}　{course.name}　{course.id}").classes("text-2xl")
+
+    with ui.grid(rows=2, columns=4).classes("w-full h-[30%]"):
+        with ui.card().classes("w-full h-full justify-center"): ui.label(f"學分 : {course.data.get("學分")}")
+        with ui.card().classes("w-full h-full justify-center"): ui.label(f"必/選 : {course.data.get("必/選")}")
+        with ui.card().classes("w-full h-full justify-center"): ui.label(f"修課人數 : {course.data.get("修課人數")}")
+        with ui.card().classes("w-full h-full justify-center"): ui.label(f"撤選人數 : {course.data.get("撤選人數")}")
+        with ui.card().classes("w-full h-full justify-center"): ui.label(f"教師 : {course.data.get("教師")}")
+        with ui.card().classes("w-full h-full justify-center"): ui.label(f"班級 : {course.data.get("班級")}")
+        with ui.card().classes("w-full h-full justify-center col-span-2"): ui.label(f"備註 : {course.data.get("備註")}")
+
+    with ui.grid(rows=1, columns=2).classes("w-full h-[15%]"):
+        ui.button("課程資訊", color="white").classes("w-full h-full text-lg")
+        ui.button("i 學園檔案", color="white").classes("w-full h-full text-lg")
+
+    with ui.card().classes("w-full h-[40%]"):
+            ui.label("期待一下")
     
-    ui.label(course.name).classes("text-4lg")
+
 
 
 @ui.refreshable
-def render_right_panel(render_type: Literal["home", "course"], arg: Any):
-    if render_type == "home":
-        render_home()
+async def render_right_panel(render_type: Literal["default", "course"], arg: Any):
+    if render_type == "default":
+        render_default()
     elif render_type == "course":
-        render_course(arg)
+        await render_course(arg)
