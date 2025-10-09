2263ef1f2483f588f9e2e64e75143ccbdb36bfef
gab61201 <gab61201@gmail.com>
2025-10-10 01:59:14 +0800

    feat(right panel): improve course rendering with detailed information and structured layout

--- views/home_components/right_panel.py
+++ views/home_components/right_panel.py
@@ -16,27 +16,38 @@ async def render_course(course: Course):
     await course.fetch_description()
     loading.delete()
 
-    with ui.row().classes("w-full h-[10%] flex items-center"):
-        ui.label(f"{course.seme}　{course.name}　{course.id}").classes("text-2xl")
+    with ui.row().classes("w-full h-[10%]"):
+        ui.space().classes("w-[10%]")
+        ui.label(f"{course.seme[:3]} - {course.seme[-1]}").classes("text-2xl w-[10%]")
+        ui.label(f"{course.name}").classes("text-2xl w-[60%]")
+        ui.label(f"{course.id}").classes("text-2xl w-[20%]")
 
     with ui.grid(rows=3, columns=4).classes("w-full h-[40%]"):
-        with ui.card().classes("w-full h-full justify-center rounded-xl"): ui.label(f"學分 : {course.data.get("學分")}")
-        with ui.card().classes("w-full h-full justify-center rounded-xl"): ui.label(f"必/選 : {course.data.get("必/選")}")
-        with ui.card().classes("w-full h-full justify-center rounded-xl"): ui.label(f"修課人數 : {course.data.get("修課人數")}")
-        with ui.card().classes("w-full h-full justify-center rounded-xl"): ui.label(f"撤選人數 : {course.data.get("撤選人數")}")
-        with ui.card().classes("w-full h-full justify-center rounded-xl"): ui.label(f"教師 : {course.data.get("教師")}")
-        with ui.card().classes("w-full h-full justify-center rounded-xl"): ui.label(f"班級 : {course.data.get("班級")}")
-        with ui.card().classes("w-full h-full justify-center rounded-xl col-span-2 py-1"): ui.label(f"備註 : {course.data.get("備註")}")
+        with ui.card().classes("w-full h-full justify-center rounded-xl"):
+            ui.label(f"學分 : {course.data.get("學分")}")
+        with ui.card().classes("w-full h-full justify-center rounded-xl"):
+            ui.label(f"必/選 : {course.data.get("必/選")}")
+        with ui.card().classes("w-full h-full justify-center rounded-xl"):
+            ui.label(f"修課人數 : {course.data.get("修課人數")}")
+        with ui.card().classes("w-full h-full justify-center rounded-xl"):
+            ui.label(f"撤選人數 : {course.data.get("撤選人數")}")
+        with ui.card().classes("w-full h-full justify-center rounded-xl"):
+            ui.label(f"教師 : {course.data.get("教師")}")
+        with ui.card().classes("w-full h-full justify-center rounded-xl"):
+            ui.label(f"班級 : {course.data.get("班級")}")
+        with ui.card().classes("w-full h-full justify-center rounded-xl col-span-2 py-1"):
+            ui.label(f"備註 : {course.data.get("備註")}")
 
-        ui.button("課程資訊", color="white").classes("w-full h-full text-lg rounded-xl")
+        ui.button("課程資訊", color="white").classes("w-full h-full text-lg rounded-xl")\
+            .on_click(lambda: render_right_panel.refresh("description", course))
         ui.button("課程錄影", color="white").classes("w-full h-full text-lg rounded-xl")
         ui.button("i學園公告", color="white").classes("w-full h-full text-lg rounded-xl")
         ui.button("i學園檔案", color="white").classes("w-full h-full text-lg rounded-xl")
 
-    user_notes_dict = app.storage.general.setdefault(user.student_id, {})
     
 
     with ui.card().tight().classes("w-full h-[50%] rounded-lg"):
+        user_notes_dict = app.storage.general.setdefault(user.student_id, {})
 
         def close_edit():
             e.set_visibility(False)
@@ -55,8 +66,9 @@ async def render_course(course: Course):
 
         
 
-def render_description():
-    ...
+def render_description(course:Course):
+    ui.button(icon="keyboard_arrow_left").classes("text-lg text-black bg-white").props("rounded flat")\
+        .on_click(lambda: render_right_panel.refresh("course", course))
 
 
 def render_ischool():
@@ -70,6 +82,6 @@ async def render_right_panel(render_type: Literal["default", "course", "descript
     elif render_type == "course":
         await render_course(arg)
     elif render_type == "description":
-        ...
+        render_description(arg)
     elif render_type == "ischool":
         ...
