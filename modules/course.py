9f750b42af8cee0a9b1ed25a10c1666723534aea
gab61201 <gab61201@gmail.com>
2025-10-17 02:20:26 +0800

    fix(video): 解決影片無法撥放的問題(SCORM_loadCA.php 解析錯誤)

--- modules/course.py
+++ modules/course.py
@@ -121,7 +121,7 @@ class Course:
         read_key = read_key_html.group(1)
 
         all_videos_html = await self.scraper.session.get('https://istudy.ntut.edu.tw/learn/path/SCORM_loadCA.php')
-        all_video_href = re.findall(r'<resource identifier="(.+?)" type="webcontent" href="(.+?)"/>', all_videos_html.text)
+        all_video_href = re.findall(r'<resource identifier="(.+?)".+?href="(.+?)"/>', all_videos_html.text)
         href = ''
         for v in all_video_href:
             if identifier == 'I_' + v[0]:
