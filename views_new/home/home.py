27f91f8e739f08df727f76f37bef862aa636a710
gab61201 <gab61201@gmail.com>
2025-10-23 11:19:04 +0800

    feat(timetable): 依照使用者課表大小渲染課表

---
+++ views_new/home/home.py
@@ -0,0 +1,33 @@
+from nicegui import app, ui
+from .home_tab import render_home_tab
+
+@ui.page('/home', title='NTUT iSchoolMate')
+async def home_route():
+    user = getattr(app, 'user')
+    ui.add_head_html('<style>body { background-color: #f3f4f6 !important; }</style>')
+
+    with ui.left_drawer().props('width=180'):
+        with ui.tabs().classes('w-full text-gray-600')\
+            .props("vertical indicator-color=transparent active-color=blue-9 active-bg-color=sky-100") as tabs:
+            ui.tab('home_tab', '首頁').classes("text-lg rounded-md")#.style("border-radius: 100000px;")
+            ui.tab('course_list_tab', '課程清單').classes("text-lg rounded-md")
+            ui.tab('course_search_tab', '課程查詢').classes("text-lg rounded-md")
+            ui.tab('search_empty_classroom_ui', '找空教室').classes("text-lg rounded-md")
+            ui.tab('schedule_tab', '行事曆').classes("text-lg rounded-md")
+            ui.tab('student_info_tab', '個人資訊').classes("text-lg rounded-md")
+
+
+    with ui.tab_panels(tabs, animated=False, value='home_tab')\
+    .classes('w-full h-[calc(100vh-32px)] bg-gray-200 shadow-lg').props("vertical") as panel:
+        with ui.tab_panel('home_tab').classes('p-0'):
+            await render_home_tab(user)
+        with ui.tab_panel('course_list_tab'):
+            ui.label('2')
+        with ui.tab_panel('course_search_tab'):
+            ui.label('3')
+        with ui.tab_panel('search_empty_classroom'):
+            ui.label('4')
+        with ui.tab_panel('schedule_tab'):
+            ui.label('5')
+        with ui.tab_panel('student_info_tab'):
+            ui.label('6')
--- views_new/home/home_tab.py
+++ views_new/home/home_tab.py
@@ -4,34 +4,59 @@ from modules.semester import Semester
 from modules.course import Course
 
 
+def get_timetable_size(table):
+    days = 5
+    lessons = 9
+
+    saturday = [table[i][1] for i in range(1, 13)]
+    sunday = [table[i][-1] for i in range(1, 13)]
+    if any(saturday) or any(sunday):
+        days = 7
+    
+    if any(table[12][1:]): #C
+        lessons = 12
+    elif any(table[11][1:]): #B
+        lessons = 11
+    elif any(table[10][1:]): #A
+        lessons = 10
+    return days, lessons
+    
+
+
+
 async def render_home_tab(user: UserManager):
 
     @ui.refreshable
     async def render_timetable(seme: Semester):
         timetable = seme.timetable
-        for hour in timetable:
-            for lesson in hour:
-                if lesson == 'menu':
-                    continue
-                elif type(lesson) == str:
-                    with ui.card().classes('w-full h-full justify-center items-center'):
-                        ui.label(lesson)
-                elif type(lesson) == Course:
-                    ui.button(lesson.name).classes('w-full h-full justify-center items-center p-0')
-                else: 
-                    ui.card().classes('w-full h-full justify-center items-center')
+        days, lessons = get_timetable_size(timetable)
 
-    with ui.row().classes('w-full h-full p-0 justify-between'):
-
-
-        with ui.grid(columns='minmax(0, 1fr)'+' 2fr'*7 , rows='1fr'+' 2fr'*12).classes('w-[49%] h-full gap-1 p-0'):
+        with ui.grid(columns='minmax(0, 1fr)'+' 2fr'*days , rows='1fr'+' 2fr'*lessons).classes('w-[49%] h-full gap-1 p-0'):
 
             with ui.button(icon='menu', color="orange-300").classes("w-full h-full text-black"):
                 with ui.menu().props('auto-close'):
                     for seme in user.semesters:
                         ui.menu_item(seme.semester, on_click=lambda s=seme:render_timetable.refresh(s))
-            if user.semesters:
-                await render_timetable(user.semesters[0])
+
+            for row in range(lessons + 1):
+                for column in range(days + 1 if days == 7 else days + 2):
+                    cell = timetable[row][column]
+                    if days == 5 and column == 1:  # skip sunday
+                        continue
+                    elif cell == 'menu':
+                        continue
+                    elif type(cell) == str:
+                        with ui.card().classes('w-full h-full justify-center items-center'):
+                            ui.label(cell)
+                    elif type(cell) == Course:
+                        ui.button(cell.name).classes('w-full h-full justify-center items-center p-0')
+                    else: 
+                        ui.card().classes('w-full h-full justify-center items-center')
+
+
+    with ui.row().classes('w-full h-full p-0 justify-between'):
+        if user.semesters:
+            await render_timetable(user.semesters[0])
 
 
         with ui.card().classes('w-[49%] h-full'):
